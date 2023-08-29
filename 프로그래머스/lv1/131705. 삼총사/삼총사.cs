0884831e09ce3670ef52ef1f1a1a05bc04f35984
Penguin.Kim <cutlake2000@naver.com>
2023-08-29 09:51:38 +0900

    [level 1] Title: 삼총사, Time: 0.18 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] number)
+    {
+        int answer = 0;
+        int length = number.Length;
+
+        for (int i = 0; i < length - 2; i++)
+        {
+            for (int j = i + 1; j < length - 1; j++)
+            {
+                for (int k = j + 1; k < length; k++)
+                {
+                    if ((number[i] + number[j] + number[k]) == 0)
+                    {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
