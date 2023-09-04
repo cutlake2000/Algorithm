eba274040758bf28b36b2e06dd8afe28d34660fd
Penguin.Kim <cutlake2000@naver.com>
2023-09-04 13:58:09 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+	int A = 0, B = 0;
+	scanf("%d %d", &A, &B);
+	if (A > B)
+	{
+		printf (">");
+	}
+	else if (A < B)
+	{
+		printf ("<");
+	}
+	else
+	{
+		printf ("==");
+	}
+	return 0;
+}
